de822feae306a68fbc1adb12429d9bed2b64d671
zijunt <zijunt@gmail.com>
2012-03-20 01:19:11 -0700

    mac 319

---
+++ MyManager/admin/views/mymanager/tmpl/edit.php
@@ -0,0 +1,20 @@
+<?php
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+JHtml::_('behavior.tooltip');
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_mymanager&layout=edit&id='.(int) $this->item->id); ?>"
+      method="post" name="adminForm" id="mymanager-form">
+	<fieldset class="adminform">
+		<legend><?php echo JText::_( 'COM_MYMANAGER_MYMANAGER_DETAILS' ); ?></legend>
+		<ul class="adminformlist">
+<?php foreach($this->form->getFieldset() as $field): ?>
+			<li><?php echo $field->label;echo $field->input;?></li>
+<?php endforeach; ?>
+		</ul>
+	</fieldset>
+	<div>
+		<input type="hidden" name="task" value="mymanager.edit" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+</form>
